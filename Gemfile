ebaa79d8280a768c9a0003eb6436320fc3ee7dee
Daniel Kehoe <daniel@danielkehoe.com>
2014-04-04 15:08:17 -0700

    build using rails_apps_composer 2.4.40

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'devise'
 gem 'pundit'
 group :development do
   gem 'better_errors'
-  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
+  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :mri_21, :rbx]
   gem 'quiet_assets'
   gem 'rails_layout'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode.
-  config.action_mailer.perform_deliveries = false
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 
 
   # Adds additional error checking when serving assets at runtime.
