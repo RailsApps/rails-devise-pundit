ee21a06d9866005d82189be633857b3ceb1dc539
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-06 18:22:36 -0700

    build using rails_apps_composer 2.6.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     unless current_user.admin?
       unless @user == current_user
-        redirect_to root_path, :alert => "Access denied."
+        redirect_to :back, :alert => "Access denied."
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-RailsDevisePundit::Application.routes.draw do
+Rails.application.routes.draw do
   root :to => "visitors#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
