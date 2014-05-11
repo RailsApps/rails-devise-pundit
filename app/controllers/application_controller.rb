3fa77485077593bf33944997488a0431a63bd68c
Daniel Kehoe <daniel@danielkehoe.com>
2014-05-11 08:19:53 -0700

    build using rails_apps_composer 2.6.6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
