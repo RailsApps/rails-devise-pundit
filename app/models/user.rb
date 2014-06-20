38c51436af3cc729a6a715c1c178bf7e56ac4fae
Daniel Kehoe <daniel@danielkehoe.com>
2014-06-20 16:28:17 -0700

    reformat user.rb file as generated by Rails Composer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
@@ -10,4 +6,8 @@ class User < ActiveRecord::Base
     self.role ||= :user
   end
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
